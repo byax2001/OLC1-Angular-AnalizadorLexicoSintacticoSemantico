62037ed0f2694f4941580008d8a9c0b9cbfad02d
Moonbbyte <63923585+Moonbbyte@users.noreply.github.com>
2022-06-03 23:11:15 -0600

    1

---
+++ Lfscript/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './PagesWeb/principal/principal.component';
+
+
+const routes: Routes = [
+  {path:'', component: PrincipalComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Lfscript/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './PagesWeb/principal/principal.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrincipalComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
